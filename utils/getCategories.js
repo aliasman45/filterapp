ccda27a5e1112f0d3d8957ec14b137cdfcec4b55
aliasman <aliasman45@gmail.com>
2023-07-29 16:53:18 +0200

    deleted unneccesry files

--- utils/getCategories.js
+++
@@ -1,6 +0,0 @@
-import { getData } from "./getData";
-export async function uniqueCategories() {
-  const data = await getData();
-  // const categories = data?.map((item) => item.category);
-  // return Array.from(new Set(categories));
-}
